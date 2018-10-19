5e8f288f2a80846a85c3218b88121f8a789d5bb1
ddody <laccooon@gmail.com>
2018-10-20 02:23:34 +0900

    add cert

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const problemsRouter = require('./routes/problems');
